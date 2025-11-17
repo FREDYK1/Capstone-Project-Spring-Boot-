efa6c731272bc181264615c6d3e15655e3306217
FREDYK1 <fkwekukankam@gmail.com>
2025-11-17 09:51:26 +0000

    Initial Commit

---
+++ turntabl/src/main/java/com/capstone/turntabl/TurntablApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.turntabl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurntablApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurntablApplication.class, args);
+	}
+
+}
